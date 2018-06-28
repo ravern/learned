e64bc301d557f1eedd375ad10c3a94e34d48b251
Ravern Koh <ravernkoh@gmail.com>
2018-06-28 16:15:31 +0800

    Add devise and login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
+
+  devise_scope :user do
+    get 'users/login', to: 'devise/sessions#new'
+  end
 end
