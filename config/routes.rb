bbbe621b62df8c89ad486c184c71ecd18310126b
Ravern Koh <ravernkoh@gmail.com>
2018-07-01 02:19:38 +0800

    Add login and registration forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
 
-  devise_for :users, skip: [:sessions, :passwords], path_names: { sign_up: 'new' }
+  devise_for :users, skip: [:sessions, :registrations], path_names: { sign_up: 'new' }
   as :user do
-    get 'sessions/new', to: 'devise/sessions#new', as: :new_user_session
-    post 'sessions', to: 'devise/sessions#create', as: :user_session
-    delete 'sessions', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'sessions/new', to: 'users/sessions#new', as: :new_user_session
+    post 'sessions', to: 'users/sessions#create', as: :user_session
+    delete 'sessions', to: 'users/sessions#destroy', as: :destroy_user_session
+
+    resource :user, controller: 'devise/registrations', as: :user_registration
+    get 'user/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
   end
 end
