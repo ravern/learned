78df47de83059b0bff85c40233b36a2205de590f
Ravern Koh <ravernkoh@gmail.com>
2018-07-01 11:26:23 +0800

    Finish Rails routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
 
-  devise_for :users, skip: [:sessions, :registrations], path_names: { sign_up: 'new' }
+  devise_for :users, skip: [:sessions, :registrations, :passwords], path_names: { sign_up: 'new' }
   as :user do
     get 'sessions/new', to: 'users/sessions#new', as: :new_user_session
     post 'sessions', to: 'users/sessions#create', as: :user_session
     delete 'sessions', to: 'users/sessions#destroy', as: :destroy_user_session
 
-    resource :user, controller: 'devise/registrations', as: :user_registration
-    get 'user/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
+    resource :user, controller: 'users/registrations', as: :user_registration
+    get 'user/cancel', to: 'users/registrations#cancel', as: :cancel_user_registration
+
+    resource :password, path: 'user/password', controller: 'users/passwords', as: :user_password, except: [:destroy]
   end
 end
